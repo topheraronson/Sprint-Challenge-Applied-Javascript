2f35b2a07f06f5ce45f46dd7c339161be1ca457f
Christopher Aronson <christopheraronson@icloud.com>
2020-03-18 19:49:52 -0700

    Created Card component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,42 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+function Card(data) {
+    
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const authorContainer = document.createElement('div')
+    const imageContainer = document.createElement('div')
+    const image = document.createElement('img')
+    const author = document.createElement('span')
+
+    headline.textContent = data.headline
+    image.src = data.authorPhoto
+    author.textContent = `By ${data.authorName}`
+    
+    card.classList.add('card')
+    headline.classList.add('headline')
+    authorContainer.classList.add('author')
+    imageContainer.classList.add('img-container')
+
+    imageContainer.append(image)
+    authorContainer.append(imageContainer, author)
+    card.append(headline, authorContainer)
+
+    return card
+}
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(res => {
+        const cardsContainer = document.querySelector('.cards-container')
+
+        Object.keys(res.data.articles).forEach((key, index) => {
+            res.data.articles[key].forEach(article => {
+                cardsContainer.appendChild(Card(article))
+            })
+        })
+    })
+    .catch(error => {
+        console.log('Error!!', error)
+    })
