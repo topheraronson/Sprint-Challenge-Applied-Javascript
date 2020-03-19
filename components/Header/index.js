e3ef8a8b77ebd9b2a715eb08cc93baee82ff5772
Christopher Aronson <christopheraronson@icloud.com>
2020-03-18 19:10:45 -0700

    Created Header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,24 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const header = document.createElement('div')
+    const date = document.createElement('span')
+    const title = document.createElement('h1')
+    const temp = document.createElement('span')
+
+    date.textContent = new Date().toDateString()
+    title.textContent = 'Lambda Times'
+    temp.textContent = '98°'
+
+    header.classList.add('header')
+    date.classList.add('date')
+    temp.classList.add('temp')
+
+    header.append(date, title, temp)
+
+    return header
+}
+
+const header = document.querySelector('.header-container')
+header.appendChild(Header())
